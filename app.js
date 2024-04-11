029810b29f27939f80985c6b5676a43284bea312
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2024-04-11 11:12:54 -0300

    added 404 page

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req, res) => {
+  res.status(404).send("<h1>404 error: page not found");
+});
 
 app.listen(3000);
