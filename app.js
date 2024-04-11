7ce852497ce11f6958259b275cb5b8e3862cb8fc
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2024-04-11 16:16:29 -0300

    used pug to output dynamic content

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require("body-parser");
 
 const path = require("path");
 
+app.engine("pug", require("pug").__express);
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const path = require("path");
-const rootDir = require("../util/path");
+// const path = require("path");
+// const rootDir = require("../util/path");
 
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop", { prods: adminData.products, docTitle: "Shop" });
   console.log("shop.js", adminData.products);
 });
 
