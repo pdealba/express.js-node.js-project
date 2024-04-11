acd5336d78c11f12c5eddd30013eacc70fc1f1ed
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2024-04-11 19:33:48 -0300

    final commit for pug files

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
+  const products = adminData.products;
   res.render("shop", {
-    prods: adminData.products,
+    prods: products,
     path: "/",
     pageTitle: "Shop",
   });
